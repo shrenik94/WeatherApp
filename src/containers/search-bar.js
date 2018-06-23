c0625f64ca49e6ba7bf4249ff8ca3a5dd27f93bb
Shrenik <30378730+shrenik94@users.noreply.github.com>
2018-06-23 14:28:22 -0400

    Added weather API and middleware

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+
+        //Important : if a call back has a reference to this you need to bind to context
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return(
-            <form className = "input-group">
-                <input />
+            <form onSubmit={this.onFormSubmit} className = "input-group">
+                <input
+                    placeholder="Get a five day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                <span className="input-group-btn">
                 <button type="submit" className="btn btn-secondary">Submit</button>
                </span>
